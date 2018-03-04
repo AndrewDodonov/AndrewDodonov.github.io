ff47fb83b3e7610fce49d2877c8e522473774861
AndrewDodonov <35918395+AndrewDodonov@users.noreply.github.com>
2018-03-04 03:54:47 +0300

    Add files via upload

---
+++ untitled15new/css/script.js
@@ -0,0 +1,6 @@
+
+var menu = document.getElementsByClassName('nav_chr')[0];
+var burger = document.getElementsByClassName('burger_menu')[0];
+menu.addEventListener('click', function () {
+    burger.style.display='block';
+})
