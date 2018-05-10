91be6aebe3c5f30a6ae2b4618261d8324c8ebf7d
AndrewDodonov <35918395+AndrewDodonov@users.noreply.github.com>
2018-05-11 00:20:42 +0300

    Add files via upload

---
+++ Rovus/js/script.js
@@ -0,0 +1,12 @@
+$(".burger").click(function () {
+    $('.line_one_5').toggleClass('rotate_one_5');
+    $('.line_two_5').toggleClass('rotate_two_5');
+    $('.line_four_5').toggleClass('rotate_four_5');
+    $('.line_three_5').toggleClass('go_three_5');
+    $('.line_one_5').toggleClass('rotate_up');
+    $('.line_three_5').toggleClass('rotate_down');
+    $('.burger_menu').toggleClass('show')
+
+});
+
+
