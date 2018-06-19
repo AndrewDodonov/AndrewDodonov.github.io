a61706bfdfc2d60cf241bd479de7d14177064b46
AndrewDodonov <35918395+AndrewDodonov@users.noreply.github.com>
2018-06-19 12:56:39 +0300

    Add files via upload

--- x-hose/js/script.js
+++ x-hose/js/script.js
@@ -2,4 +2,13 @@ $(".burger").click(function () {
     $('.line_one').toggleClass('rotate_one');
     $('.line_two').toggleClass('rotate_two');
     $('.line_three').toggleClass('go_three')
+    $('.burger_menu').toggleClass('show')
+})
+
+$('.menu_hrefs_burger').click(function () {
+    $('.line_one').toggleClass('rotate_one');
+    $('.line_two').toggleClass('rotate_two');
+    $('.line_three').toggleClass('go_three')
+    $('.burger_menu').toggleClass('show')
+
 })
