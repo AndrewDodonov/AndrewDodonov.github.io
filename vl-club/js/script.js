3c3a2d9323cea35d0a247d01974df1c9019a2369
AndrewDodonov <35918395+AndrewDodonov@users.noreply.github.com>
2018-06-25 21:13:28 +0300

    Add files via upload

---
+++ vl-club/js/script.js
@@ -0,0 +1,13 @@
+
+
+setTimeout(function () {
+    $('.consultation').css({top:'200px'})
+}, 2000)
+
+
+$('.consultation_href').click(function () {
+    $(".consultation").addClass('hide')
+})
+
+
+
