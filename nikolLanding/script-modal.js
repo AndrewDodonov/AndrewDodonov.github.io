c92986db3c888009544ddcac517fe17f95d16922
AndrewDodonov <35918395+AndrewDodonov@users.noreply.github.com>
2018-07-13 15:34:10 +0300

    Add files via upload

---
+++ nikolLanding/script-modal.js
@@ -0,0 +1,67 @@
+$('.button').click(function(){
+    var buttonId = $(this).attr('id');
+    $('#modal-container').removeAttr('class').addClass(buttonId);
+    $('body').addClass('modal-active');
+})
+
+$('.button').click(function(){
+    var buttonId = $(this).attr('id');
+    $('#modal-container-1').removeAttr('class').addClass(buttonId);
+    $('body').addClass('modal-active');
+})
+
+$('.button').click(function(){
+    var buttonId = $(this).attr('id');
+    $('#modal-container-2').removeAttr('class').addClass(buttonId);
+    $('body').addClass('modal-active');
+})
+
+$('.button').click(function(){
+    var buttonId = $(this).attr('id');
+    $('#modal-container-3').removeAttr('class').addClass(buttonId);
+    $('body').addClass('modal-active');
+})
+
+
+
+
+
+$('.modal_back').click(function(){
+    $('#modal-container').addClass('out');
+    $('body').removeClass('modal-active');
+});
+
+$('.modal_back').click(function(){
+    $('#modal-container-1').addClass('out');
+    $('body').removeClass('modal-active');
+});
+
+$('.modal_back').click(function(){
+    $('#modal-container-2').addClass('out');
+    $('body').removeClass('modal-active');
+});
+
+$('.modal_back').click(function(){
+    $('#modal-container-3').addClass('out');
+    $('body').removeClass('modal-active');
+});
+
+$('.chr').click(function(){
+    $('#modal-container').addClass('out');
+    $('body').removeClass('modal-active');
+});
+
+$('.chr').click(function(){
+    $('#modal-container-1').addClass('out');
+    $('body').removeClass('modal-active');
+});
+
+$('.chr').click(function(){
+    $('#modal-container-2').addClass('out');
+    $('body').removeClass('modal-active');
+});
+
+$('.chr').click(function(){
+    $('#modal-container-3').addClass('out');
+    $('body').removeClass('modal-active');
+});
---
+++ nikolLanding/send.php
@@ -0,0 +1,24 @@
+﻿<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'jackvorobey308@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Заказ товара'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+<p>Форма заказа satorisan</p>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p>                          
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Отправитель <seocrafter@outlook.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+
+header('Location: https://satorisanshop.ru');
+
+
+?>
